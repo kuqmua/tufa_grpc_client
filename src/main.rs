17b0bec714d75626ada1b50b18351097ad8853eb
kuqmua <serezhashmakov@outlook.com>
2022-11-24 21:20:10 +0300

    clippy

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+#![deny(
+    clippy::indexing_slicing,
+    clippy::integer_arithmetic,
+    clippy::unwrap_used,
+    clippy::float_arithmetic
+)]
+#![allow(clippy::too_many_arguments)]
+
 use payments::bitcoin_client::BitcoinClient;
 use payments::BtcPaymentRequest;
 
