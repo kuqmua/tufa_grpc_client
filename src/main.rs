afbcf1168d1771d75b393df30e377b7dd06950e6
kuqmua <serezhashmakov@outlook.com>
2023-07-29 14:57:13 +0300

    lil tweaks

--- src/main.rs
+++ src/main.rs
@@ -6,18 +6,15 @@
 )]
 #![allow(clippy::too_many_arguments)]
 
-use payments::bitcoin_client::BitcoinClient;
-use payments::BtcPaymentRequest;
-
 pub mod payments {
     tonic::include_proto!("payments");
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut client = BitcoinClient::connect("http://[::1]:50051").await?;
+    let mut client = payments::bitcoin_client::BitcoinClient::connect("http://[::1]:50051").await?;
 
-    let request = tonic::Request::new(BtcPaymentRequest {
+    let request = tonic::Request::new(payments::BtcPaymentRequest {
         from_addr: "123456".to_owned(),
         to_addr: "654321".to_owned(),
         amount: 22,
